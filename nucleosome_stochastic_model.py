a70070c04ab4763b1b6ddd148227a8166a47e301
daniel luo <danielluo1143@gmail.com>
2026-04-01 16:51:34 +0100

    added ability to track dwell_time

--- nucleosome_stochastic_model.py
+++ nucleosome_stochastic_model.py
@@ -1,13 +1,12 @@
 import numpy as np
-import pandas as pd
 import seaborn as sns
+import polars as pl
 import matplotlib.pyplot as plt
-import json
 from matplotlib.colors import ListedColormap
 from matplotlib.patches import Patch
 
 class ClusterModel():
-    def __init__(self, model_param, model_var, model_binding_sites, sim_max_time):
+    def __init__(self,model_param: dict, model_var: dict, model_binding_sites: int, sim_max_time: int):
         self.t_max: int = sim_max_time
         self.sox2_model_parameters: dict = model_param
         self.sox2_model_variables: dict = model_var
@@ -39,10 +38,12 @@ class ClusterModel():
         #               [  0   1   0]
         #               ...
         self.bulk_states = np.array(self.bulk_states)  
-        df = pd.DataFrame({
+        df = pl.DataFrame(
+            {
             'Time (s)': self.times, 
             'mRNA Molecules': self.bulk_states[:, 2]
-        })
+            } 
+        )
         plt.figure(figsize=(10, 5))
         ax = sns.lineplot(
             data=df, 
@@ -61,17 +62,15 @@ class ClusterModel():
     # t1. calculate time spent bound generally   
     # t2. calculate time spent bound for specific states
     def return_residence_time(self):
-        # take time spent in the state subtract the next state
-        # taking the last state, subtract ts from tmax time
-        # note: all valuse in scientific notation e.g. 1 is 1.0x10**1
-        np.savetxt("time-log.txt", self.residence_time_states)
-        
+        # for each site int64 calculate the sum of the dwell time
+        self.residence_time_states = self.simulation_site_dwell_time_df.group_by("dwell_site").agg(pl.col("dwell_time").sum())
+        np.savetxt("time-log.txt", self.residence_time_states.sort(by="dwell_time", descending=True), fmt="%g", header = "dwell_site/dwell_time",delimiter=", ")
 
     def run_sox2_sim(self):
         k_prod_s, k_deg_s, k_prod_m, k_deg_m, k_bind, k_unbind, k_hop, lambda_hop = self.sox2_model_variables
         t = 0.0
         binding_sites_array = np.zeros(self.n_binding_sites, dtype=np.int8) # 0 = free, 1 = bound
-        state = np.array(self.sox2_model_parameters, dtype=np.int32) # track bulk molecule counts
+        parameter_states = np.array(self.sox2_model_parameters, dtype=np.int32) # track bulk molecule counts
         bridged_to = np.full(self.n_binding_sites, -1, dtype=np.int8) # track bridged sites
         indices = np.arange(self.n_binding_sites)
         # initialise 2d matrix where rwo and columns are there corresponding nucleosome number
@@ -90,13 +89,13 @@ class ClusterModel():
         ], dtype=np.int32)
         
         self.times = [0.0]
-        self.bulk_states = [state.copy()]                  
+        self.bulk_states = [parameter_states.copy()]                  
         self.spatial_states = [binding_sites_array.copy()]    
         self.residence_time_states = []
         
         # issue: need to update bridged_to array at the end, ensure sox2 isnt accidently created when unbound is called, and more
         while t < self.t_max:
-            sox2_free, sox2_bound, mrna_count = state
+            sox2_free, sox2_bound, mrna_count = parameter_states
             bound_vec = (binding_sites_array == 1).astype(float)
             empty_vec = (binding_sites_array == 0).astype(float)
             unbound_nucleosomes = np.sum(binding_sites_array == 0)
@@ -120,11 +119,11 @@ class ClusterModel():
             if total_prop == 0:
                 break
             r1, r2 = np.random.random(2)
-            tau = (1.0 / total_prop) * np.log(1.0 / r1)
-            t += tau
+            t_next_reaction = (1.0 / total_prop) * np.log(1.0 / r1)
+            t += t_next_reaction
             cumulative_props = np.cumsum(propensities)
             reaction_index = np.searchsorted(cumulative_props, r2 * total_prop)
-            state += stoichiometry_matrix[:, reaction_index]
+            parameter_states += stoichiometry_matrix[:, reaction_index]
             
             ## reaction logic
             if reaction_index == 1: # bind reaction
@@ -141,7 +140,7 @@ class ClusterModel():
                         # select specific transition based on probability
                         chosen_flat_idx = np.random.choice(self.n_binding_sites * self.n_binding_sites, p=flat_probs)
                         # convert said index into a 2d array
-                        source_idx, target_idx = np.unravel_index(chosen_flat_idx, (self.n_binding_sites, self.n_binding_sites))
+                        dwell_site, next_site = np.unravel_index(chosen_flat_idx, (self.n_binding_sites, self.n_binding_sites))
                         
                         # need to add a state where it is hopped to another site, but also it also remains at the original site as well.
                         # so effectively 1 sox2 can occupy two sites at once. 
@@ -150,16 +149,33 @@ class ClusterModel():
                         # sox2 when bound to two sites cannot hop to another site.
                         # or.. add a rule where you track where and when a hop has occured 
                         # you can feed this into the list of valid transitions
-                        binding_sites_array[source_idx] = 0
-                        binding_sites_array[target_idx] = 1
-                        self.residence_time_states.append([source_idx, target_idx])
-            
+                        binding_sites_array[dwell_site] = 0
+                        binding_sites_array[next_site] = 1
+                        self.residence_time_states.append([dwell_site, t_next_reaction])
+
             self.times.append(t)
-            self.bulk_states.append(state.copy())
+            self.bulk_states.append(parameter_states.copy())
             self.spatial_states.append(binding_sites_array.copy())
-          
-        return self.times, np.array(self.bulk_states), np.array(self.spatial_states)
-sox2_model_variables = {
+        
+        self.simulation_parameter_df = pl.DataFrame(
+            {
+                "time": self.times,
+                "sox2_free": [state[0] for state in self.bulk_states],
+                "sox2_bound": [state[1] for state in self.bulk_states],
+                "mrna": [state[2] for state in self.bulk_states],
+            }
+        )
+        dwell_sites = [state[0] for state in self.residence_time_states]
+        dwell_times = [state[1] for state in self.residence_time_states]
+        self.simulation_site_dwell_time_df = pl.DataFrame(
+            {
+                "dwell_site": dwell_sites,
+                "dwell_time": dwell_times
+            }, 
+            schema={"dwell_site": pl.Int64, "dwell_time": pl.Float64}
+        )
+        return self.simulation_parameter_df, self.simulation_site_dwell_time_df
+sox2_model_variables_dict = {
     "k_prod_s": 0,
     "k_deg_s": 0,
     "k_prod_m": 1,
@@ -169,17 +185,17 @@ sox2_model_variables = {
     "k_hop": 1,
     "lambda_hop": 1
 }
-sox2_model_parameters = {
+sox2_model_parameters_dict = {
     "sox2_free": 1,
     "sox2_bound": 0,
     "mrna": 0
 }
-sox2_simulation_parameters = [sox2_model_parameters[key] for key in sox2_model_parameters]
-sox2_simulation_variables = [sox2_model_variables[key] for key in sox2_model_variables]
+sox2_simulation_parameters = [sox2_model_parameters_dict[key] for key in sox2_model_parameters_dict]
+sox2_simulation_variables = [sox2_model_variables_dict[key] for key in sox2_model_variables_dict]
 
-## model calling
+## Model Calling
 model_call = ClusterModel(sox2_simulation_parameters, sox2_simulation_variables, model_binding_sites=50, sim_max_time=500)
-ClusterModel.run_sox2_sim(model_call)
-ClusterModel.plot_mrna_history(model_call)
-ClusterModel.plot_nucleosome_occupancy_history(model_call)
-ClusterModel.return_residence_time(model_call)
+model_call.run_sox2_sim()
+model_call.plot_mrna_history()
+model_call.plot_nucleosome_occupancy_history()
+model_call.return_residence_time()
