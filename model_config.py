0e7cad00d9c72be51dd1168c815fb2ce083da9ab
dan <danielluo1143@gmail.com>
2026-06-02 22:02:59 +0100

    fix: remove hardcoded reference to folder

--- model_config.py
+++ model_config.py
@@ -1,11 +1,11 @@
 model_var = {
-    "sox2_monomer_free": 9, 
-    "nanog_monomer_free": 1, 
+    "sox2_monomer_free": 0, 
+    "nanog_monomer_free": 0, 
     "sox2_monomer_bound": 0, 
     "nanog_monomer_bound": 0, 
     "nanog_sox2_dimer_bound": 0, 
     "nanog_nanog_dimer_bound": 0,
-    "nanog_sox2_dimer_free": 0,
+    "nanog_sox2_dimer_free": 1,
     "nanog_nanog_dimer_free": 0,
     "nanog_sox2_dimer_single_bound": 0,
     "nanog_nanog_dimer_single_bound": 0,
@@ -16,7 +16,7 @@ model_param = {
     "k_s_in": 0, "k_s_out": 0,
     "k_n_in": 0, "k_in_out": 0, 
     "k_bind_s": 1.0, "k_unbind_s": 0.06,
-    "k_bind_n": 1.0, "k_unbind_n": 0.2,
+    "k_bind_n": 0.1, "k_unbind_n": 0.2,
     "k_dimerise": 0,  
     "k_prod_m": 1.0,    
     "k_deg_m": 0.53, 
@@ -48,5 +48,5 @@ u_bounds = [1.0]
 dimensions = 1
 optimization = "random-cd"
 samples = 100
-out_dir = "J:/test"
+out_dir = "output"
 
