64fad5f9aead360172b6c9574e815587615cce35
daniel luo <danielluo1143@gmail.com>
2026-06-04 14:50:52 +0100

    fix: run_simulation.py now takes snakemake params

--- scripts/run_simulation.py
+++ scripts/run_simulation.py
@@ -1,20 +1,21 @@
-import os
-import src.expression_model.model_config as config
+import yaml
 from src.expression_model.model_sample import run_single_parameter_set
-
+from src.expression_model.config_default import model_param, model_var
 
 if __name__ == "__main__":
+    with open(snakemake.params.config_path, 'r') as f:
+        cfg = yaml.safe_load(f)
+        
+    rates = {**model_param, **cfg.get("rates", {})}
+    states = {**model_var, **cfg.get("initial_state", {})}
     experiment_name = snakemake.params.experiment
-
-    run_rates = config.model_param.copy()
-    model_var = config.model_var.copy()
-
+    
     run_single_parameter_set(
-        sim_max_time=1000,
+        sim_max_time=cfg.get("sim_time", 1000),
         model_binding_sites=10,
         runs=100, 
-        custom_initial_state=model_var,
-        custom_rates=run_rates,
+        custom_initial_state=states,
+        custom_rates=rates,
         param_set_id=experiment_name
     )
 
