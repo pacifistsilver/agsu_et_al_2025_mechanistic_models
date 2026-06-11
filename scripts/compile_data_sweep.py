b03186a2e61eb86a4bd827e7cf72ab09e0d32fa4
dan <danielluo1143@gmail.com>
2026-06-11 17:09:41 +0100

    refactor: parsed snakemake directory into compile data

--- scripts/compile_data_sweep.py
+++ scripts/compile_data_sweep.py
@@ -1,21 +1,23 @@
+import sys
 import os
+sys.path.insert(0, os.path.abspath("."))
+
 import json
 import polars as pl
-from eda_functions import Statistics
+from src.eda_functions import Statistics
 
 if __name__ == "__main__":
+    sweep_dirs = snakemake.input.sweep_dirs
     all_run_features = []
 
-    print(f"Compiling {len(snakemake.input)} parameter configurations...")
+    print(f"Compiling {len(sweep_dirs)} parameter configurations...")
 
-    # 1. Iterate through all the simulation.done files Snakemake tracked
-    for done_file in snakemake.input:
-        # The folder is the directory containing the .done file
-        folder = os.path.dirname(done_file)
-        param_set_id = os.path.basename(folder)
+    # 1. Iterate through all the simulation folders
+    for sweep_dir in sweep_dirs:
+        param_set_id = os.path.basename(sweep_dir)
         
         # Load the parameters saved by the simulation script
-        params_path = os.path.join(folder, "model_parameters.json")
+        params_path = os.path.join(sweep_dir, "model_parameters.json")
         with open(params_path, "r") as f:
             params_data = json.load(f)
             
@@ -24,7 +26,7 @@ if __name__ == "__main__":
 
         # Extract features using your Statistics class
         stats = Statistics.from_parquet_dir(
-            param_set_dir=folder, 
+            param_set_dir=sweep_dir, 
             total_sim_time=1000, 
             total_binding_sites=10
         )
